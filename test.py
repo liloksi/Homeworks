6920e4f33077fdcd5b6f5c9ffd00fb78e5812b9e
amir <benzooksi@gmail.com>
2023-02-18 23:57:55 +0600

    realise v1

---
+++ test.py
@@ -0,0 +1,43 @@
+# 1 задание
+
+# def div_by_5(a):
+#     if a % 5 == 0:
+#         return True
+#     else:
+#         return False
+    
+# print(div_by_5(500))
+# print(div_by_5(444))
+
+
+# 3 задание
+
+# import random
+
+# def id_generator(number):
+#     number = str(random.randint(0, 10000000000000000))
+#     print(number)
+
+# id_generator(number=5)
+
+
+
+# 4 задание
+
+# import math
+
+# def factorial(number):
+#     number = int(input('Введите число: '))
+#     result = math.factorial(number)
+#     print("Факториал", number, "это", result)
+
+# print(factorial(number=5))
+
+
+# 7 задание
+
+# def maskify(number):
+#     number = (input('Введите число: '))
+#     return "#"*(len(number)-4) + number[-4:]
+
+# print(maskify(number=1))
